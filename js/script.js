079a24deda7dcbdc72c8efb0d68727defd0c50b6
NAErshov <nikolay.ershov.a@gmail.com>
2022-07-03 15:49:08 +0300

    3.7.2022
    jQwery

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@ $(document).ready(function() {
     const item = $('.slider-item');
     const btnPrev = $('.btn-prev');
     const btnNext = $('.btn-next'); 
+    // Сумарное колличество элементов
+    const itemCount = item.length;
     // Находим ширину каждого элемента
     const itemWidth = container.width() / slidesToShow;
+    // Чтобы понять сколько нам нужно проскролить, необходимо выполнить простое умножение
+    const movePosition = slidesToScroll * itemWidth;
+
+
     // Теперь надо проставить полученную ширину элементам
     item.each(function(index, item){
         $(item).css({
@@ -16,11 +22,34 @@ $(document).ready(function() {
         });
     });
 
-    // Функции для наших кнопок
-    btnPrev.click(function(){
-        console.log 
-    });
+     // Функции для наших кнопок
     btnNext.click(function(){
-        console.log 
+        const itemLeft = itemCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+        position -= itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+
+        setPosition();
+        chechBtns();
     });
+
+    btnPrev.click(function(){
+        const itemLeft = Math.abs(position) / itemWidth;
+        position += itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+
+        setPosition();
+        chechBtns();
+    });
+
+    const setPosition = () => {
+        track.css({
+            transform: `translateX(${position}px)`
+        });
+    };
+
+    // Проверяем активны ли наши кнопки или нет
+    const chechBtns = () => {
+        btnPrev.prop('disabled', position === 0);
+        btnNext.prop('disabled',position <= -(itemCount - slidesToShow) * itemWidth);
+    };
+
+    chechBtns();
 });
